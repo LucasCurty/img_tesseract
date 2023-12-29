eaebeff4e798f8b60a29415d89b0223dd8075fd9
Lucas Curty <lukas.curty.21@gmail.com>
2023-12-29 17:50:03 -0300

    Comentando o código

--- index.py
+++ index.py
@@ -2,23 +2,26 @@ import os
 import cv2
 import pytesseract
 
+#Passando o caminho do tesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
+# Função com a logica
 def extraindo_numero(caminho_imagem):
     try:
+        #lendo o arquivo com o CV2
         canhotinho = cv2.imread(caminho_imagem)
 
-        # Verifica se a leitura da imagem foi bem-sucedida
+        # Cortando a imagem e pegando o lado direito PS: Regra separada para facilitar a leitura dos numeros a direita dos canhotos
         if canhotinho is not None:
             altura, largura, _ = canhotinho.shape
             metade_largura = largura // 2
             leitura = canhotinho[:, metade_largura:]
 
+            #lendo a imagem com o tesseract
             numeros_extraidos = pytesseract.image_to_string(leitura, lang='por', config='--oem 3 --psm 6 -l por')
             palavras = numeros_extraidos.split()
-            print(palavras)
-
-            # Encontrar a posição da palavra "No."
+            
+            # tentando encontrar a posição da palavra "No."" ou "No"
             posicao_no = None
             for i, palavra in enumerate(palavras):
                 if "No." in palavra or ("No" in palavra and "No." not in palavra):
@@ -30,21 +33,18 @@ def extraindo_numero(caminho_imagem):
             if posicao_no is not None and posicao_no + 1 < len(palavras):
                 numero_apos_no = palavras[posicao_no + 1]
 
-            return numero_apos_no
+            return numero_apos_no #retornando o texto
         else:
             print(f"Erro: Não foi possível ler a imagem: {caminho_imagem}")
-            return None
-
     except Exception as err:
         print(f"Erro encontrado: {err}")
-        return None
 
-# Carregando a imagem
-diretorio_canhotos = "Canhotos"
-lista_canhotos = os.listdir(diretorio_canhotos)
+# Informando o diretorio das imagens
+lista_canhotos = os.listdir("Canhotos")
 
+# Criando o laço de repetição para executar os arquivos
 for arquivo in lista_canhotos:
-    caminho_arquivo_original = os.path.join(diretorio_canhotos, arquivo)
+    caminho_arquivo_original = os.path.join(lista_canhotos, arquivo)
     numero_extraido = extraindo_numero(caminho_arquivo_original)
 
     if numero_extraido:
@@ -55,7 +55,7 @@ for arquivo in lista_canhotos:
         novo_nome = f'{numero_extraido}{extensao}'
         
         # Constrói o caminho completo para o novo arquivo
-        caminho_novo = os.path.join(diretorio_canhotos, novo_nome)
+        caminho_novo = os.path.join(lista_canhotos, novo_nome)
 
         # Verifica se o caminho_novo já existe
         if os.path.exists(caminho_novo):
